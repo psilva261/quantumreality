71bf4a901baa90c84b5609cd5af9fab737e0b423
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-09-24 23:59:49 +0200

    spawning

--- unityVr/Assets/GenerateObjectsInRange.cs
+++ unityVr/Assets/GenerateObjectsInRange.cs
@@ -2,21 +2,29 @@
 using System.Collections;
 
 public class GenerateObjectsInRange : MonoBehaviour {
-	public Transform prefab;
+	public CircularPathBuilder pathBuilder;
+	public Transform[] prefab;
 	public int count = 10;
 	public float radius = 10;
 	// Use this for initialization
 	void Start () {
 		for(int i = 0; i < count; i++){
-			Transform child = Instantiate(prefab);
-			child.parent = transform;	
+			int prefabI = Random.Range(0,prefab.Length);
+			Transform child     = Instantiate(prefab[prefabI]);
+			child.parent        = transform;	
 			child.localPosition = Random.insideUnitSphere;
-					
-			float magnitude = child.localPosition.magnitude;
+			float magnitude     = child.localPosition.magnitude;
 			
 			child.localPosition *= Random.Range(1, radius / magnitude);
-			
+			child.localScale = transform.localScale;
+			var pathFollowers = child.GetComponents<PathFollowingController>();
+			if(pathBuilder != null && pathFollowers.Length > 0){
+				foreach(var follower in pathFollowers){
+					follower._pathRoot = pathBuilder.transform;
+				}
+			}
 		}
+		
 	}
 	
 	// Update is called once per frame
