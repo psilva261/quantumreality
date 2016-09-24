71775815c693a13b4fd01e41029f6d7ed7a6b0db
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-09-24 16:02:17 +0200

    start collecting right stuff

---
+++ unityVr/Assets/CircularPathBuilder.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CircularPathBuilder : MonoBehaviour {
+	public int   numberOfNodes = 10;
+	public float radius = 1;
+	public Transform prefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+[CustomEditor(typeof(CircularPathBuilder))]
+public class CircularPathBuilderEditor : Editor{
+	public CircularPathBuilder Target {
+		get{ return (target as CircularPathBuilder); }
+	}
+
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector();
+		if(GUILayout.Button("Update Path")){
+			float     radius = Target.radius;
+			int       nodes  = Target.numberOfNodes;
+			Transform prefab = Target.prefab;
+
+			foreach(Transform child in Target.transform){
+				
+			}
+		}
+	}
+
+}
